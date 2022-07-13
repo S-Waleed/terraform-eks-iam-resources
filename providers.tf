8d6554a361c2662ab50eafb2909f726ee1a6357f
Waleed <masterwali0402@gmail.com>
2022-07-13 07:41:24 -0400

    try irsa module

--- providers.tf
+++ providers.tf
@@ -1,11 +1,6 @@
 provider "aws" {
   region = var.region
 
-  assume_role {
-    role_arn     = "arn:aws:iam::${var.account_id}:role/svc-terraform-role"
-    session_name = "terraform"
-  }
-
   # Common tags for all resources that accept tags
   default_tags {
     tags = {
